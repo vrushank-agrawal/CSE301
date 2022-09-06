7a6244c0d4b1b178b11fbf548e3c554a023a9149
vrushank-agrawal <vrushank.agrawal@thon.polytechnique.fr>
2022-09-06 17:10:34 +0200

    Init cse301

---
+++ vrushank_lab1.hs
@@ -0,0 +1,28 @@
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] [] = []
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x <= y
+                      then x : merge xs (y:ys)
+                      else y : merge (x:xs) ys
+
+split :: [a] -> ([a], [a])
+split [] = ([], [])
+split xs = (take half xs, drop half xs)
+           where half = length xs `div` 2
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort ys) (msort zs)
+           where
+           (ys, zs) = split (xs)
+
+
+isSorted :: Ord a => [a] -> Bool
+isSorted [] = True
+isSorted [x] = True
+isSorted (x:xs:ys) = if x<=xs
+                  then isSorted (xs:ys)
+                  else False
+
