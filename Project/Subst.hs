e95e9cbc5511a92b35f2c99f5374fad8e1a4d58d
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-17 17:03:03 +0200

    add basic subst implementation without free decl

--- Project/Subst.hs
+++ Project/Subst.hs
@@ -3,4 +3,11 @@ module Subst (subst) where
 import Expr
 
 subst :: (LExp,Var) -> LExp -> LExp
-subst = undefined
+-- definition for variable substitution
+subst (d,x) (V v) | v == x     = d
+                  | otherwise  = V v
+-- definition for expression composition substitution
+subst sub (A e1 e2) = A (subst sub e1) (subst sub e2)
+-- definition for Lambda expression substitution
+subst (d,x) (L v e) | v == x    = (L v e)
+                    | otherwise = (L v (subst (d,x) e))
