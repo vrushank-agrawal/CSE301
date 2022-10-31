feedab83d7fe0ba973b63d8df4ec17d1608ab6af
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-31 17:57:22 +0100

    finish subst

--- Project/Subst.hs
+++ Project/Subst.hs
@@ -11,9 +11,13 @@ isFree v (L v' e1)   = isFree v e1
 swap :: LExp -> Var -> Var -> LExp
 swap (V v) y z      | v == y    = (V z)
                     | otherwise = (V v)
-swap (A e1 e2) y z  = (A (swap e1 y z))
+swap (A e1 e2) y z  = (A (swap e1 y z) (swap e2 y z))
 swap (L v e) y z    | v == y    = (L z (swap e y z))
                     | otherwise = (L v (swap e y z))
+
+-- function to modify variable name
+modify :: Var -> Var
+modify v = v++"'"
 
 subst :: (LExp,Var) -> LExp -> LExp
 -- definition for variable substitution
@@ -23,5 +27,6 @@ subst (d,x) (V v) | v == x     = d
 subst dx (A e1 e2) = A (subst dx e1) (subst dx e2)
 -- definition for Lambda expression substitution
 subst (d,x) (L v e) | v == x         = (L v e)
-                    | not isFree v d = (L v (subst (d,x) e))
+                    | not (isFree v d) = (L v (subst (d,x) e))
                     | otherwise      = (L v' (subst (d,x) (swap e v v')))
+                                       where v' = modify v
