6f8d40d117e1dc59433698b0815c813dfc0888e1
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-31 16:26:47 +0100

    start subst

--- Project/Subst.hs
+++ Project/Subst.hs
@@ -2,6 +2,10 @@ module Subst (subst) where
 
 import Expr
 
+checkFree :: Var -> LExp -> Bool
+checkFree v (V v'')   = v == v''
+checkFree v (L e1 e2) = undefined
+
 subst :: (LExp,Var) -> LExp -> LExp
 -- definition for variable substitution
 subst (d,x) (V v) | v == x     = d
@@ -9,5 +13,5 @@ subst (d,x) (V v) | v == x     = d
 -- definition for expression composition substitution
 subst sub (A e1 e2) = A (subst sub e1) (subst sub e2)
 -- definition for Lambda expression substitution
-subst (d,x) (L v e) | v == x    = (L v e)
-                    | otherwise = (L v (subst (d,x) e))
+subst (d,x) (L v e) | v == x        = (L v e)
+                    | checkFree v e = (L v (subst (d,x) e))
