0f8f000c8cdc053030e4e9049aca30dc899a39f7
vrushank-agrawal <vrushank2001@gmail.com>
2022-10-31 17:35:13 +0100

    almost finish subst

--- Project/Subst.hs
+++ Project/Subst.hs
@@ -2,16 +2,26 @@ module Subst (subst) where
 
 import Expr
 
-checkFree :: Var -> LExp -> Bool
-checkFree v (V v'')   = v == v''
-checkFree v (L e1 e2) = undefined
+isFree :: Var -> LExp -> Bool
+isFree v (V v')      = v == v'
+isFree v (A e1 e2)   = isFree v e1 || isFree v e2
+isFree v (L v' e1)   = isFree v e1
+
+-- function to replace variable in LExp
+swap :: LExp -> Var -> Var -> LExp
+swap (V v) y z      | v == y    = (V z)
+                    | otherwise = (V v)
+swap (A e1 e2) y z  = (A (swap e1 y z))
+swap (L v e) y z    | v == y    = (L z (swap e y z))
+                    | otherwise = (L v (swap e y z))
 
 subst :: (LExp,Var) -> LExp -> LExp
 -- definition for variable substitution
 subst (d,x) (V v) | v == x     = d
                   | otherwise  = V v
 -- definition for expression composition substitution
-subst sub (A e1 e2) = A (subst sub e1) (subst sub e2)
+subst dx (A e1 e2) = A (subst dx e1) (subst dx e2)
 -- definition for Lambda expression substitution
-subst (d,x) (L v e) | v == x        = (L v e)
-                    | checkFree v e = (L v (subst (d,x) e))
+subst (d,x) (L v e) | v == x         = (L v e)
+                    | not isFree v d = (L v (subst (d,x) e))
+                    | otherwise      = (L v' (subst (d,x) (swap e v v')))
