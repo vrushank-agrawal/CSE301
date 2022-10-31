ef0fbd63dd40454452adeed3dbb9a2c9c35660b4
yiyaotan <yytan_work@gmail.com>
2022-10-31 19:30:42 +0100

    subexp works but redex onwards doesn't

--- Project/Eval.hs
+++ Project/Eval.hs
@@ -7,6 +7,7 @@ import Subst
 data LExpCxt = Hole | A'1 LExpCxt LExp | A'2 LExp LExpCxt | L' Var LExpCxt
   deriving (Show,Eq)
 
+test1 = (L "x" (A (V "x") (V "y")))
 -- we represent contexts "inside-out", i.e., with the parts of the
 -- context nearest to the hole at the top-level.
 -- The plugging function is defined accordingly.
@@ -23,13 +24,26 @@ type LExpPtr = (LExpCxt,LExp)
 -- mini-project page...
 
 subexp :: LExp -> [LExpPtr]
-subexp = undefined
+subexp t = focus t Hole
+  where 
+    focus :: LExp -> LExpCxt -> [LExpPtr]
+    focus (V x) c = [(c,V x)] -- base case return context of variable
+    -- recursive case for each part of expressions of an application
+    -- put the context of the hole in the list of subexpressions
+    focus (A e1 e2) c = (c, (A e1 e2)):(focus e1 (A'1 c e2)) ++ (focus e2 (A'2 e1 c))
+    -- recursive case for lambda expression
+    focus (L x e) c = (c, (L x e)): (focus e (L' x c))
+
+--ive no clue
 
 redex :: LExp -> [LExpPtr]
-redex = undefined
+redex (V x) = [] -- base case for variable
+redex (A (L x e1) e2) = [(Hole, (subst (e2,x) e1))] -- base case for application
+redex (A e1 e2) = (redex e1) ++ (redex e2) -- recursive case for application
+redex (L x e) = (redex e) -- recursive case for lambda
 
 stepBeta :: LExp -> [LExp]
-stepBeta = undefined
+stepBeta t = map (plug (fst (head (redex t))) . snd) (redex t)
 
 normalize :: LExp -> LExp
 normalize = undefined
