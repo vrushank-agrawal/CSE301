79406e91746cc68cd07abf0a405cc8c082c76c8b
vrushank-agrawal <vrushank2001@gmail.com>
2022-11-06 15:49:28 +0100

    fix conflicts

--- Project/MyLambda.hs
+++ Project/MyLambda.hs
@@ -18,58 +18,9 @@ type Flags = (Bool, String)
 undefinedVar :: Env -> LExp -> Maybe Var
 undefinedVar env t = find (\y -> lookup y env == Nothing) (free t)
 
-
-
 -- the top-level read-eval-print-loop
 repl :: IO ()
 repl = go [] (False, "norm") -- start the interpreter in an empty environment
--- <<<<<<< HEAD
---  where
---    go :: Env -> Flags -> IO ()
---    go env flags = do
---      putStr "> " -- print the prompt
---      hFlush stdout -- flush standard output
---      line <- getLine -- get a line of input
---      cmd <- readParser parseCmd line -- parse the input as a command
---      case cmd of
---        Eval t ->
---          -- execute an eval command
---          -- the expression to be evaluated cannot have any free
---          -- variables that are not defined in the environment
---          case undefinedVar env t of
---            Just y -> putStrLn ("Variable not in scope: " ++ y) >> go env flags
---            Nothing -> do
---              -- substitute for all variables defined in the environment,
---              -- in order from left to right
---              let t' = foldl (\t (x, u) -> subst (u, x) t) t env
---              -- normalize the resulting term
---              u <- if fst flags then normAll t' $ snd flags else normalize t' $ snd flags
---              -- print the result
---              mapM_ (putStrLn . prettyLExp) u
---              -- continue the REPL
---              go env flags
---        Let x t ->
---          -- execute a let command
---          case undefinedVar env t of
---            Just y -> putStrLn ("Variable not in scope: " ++ y) >> go env flags
---            Nothing -> do
---              -- continue the REPL in an environment extended with x=t
---              go ((x, t) : env) flags
---        Noop -> go env flags -- execute a no-op command, by doing nothing and continuing the REPL
---        Quit -> do
---          -- execute a quit command, by terminating the REPL
---          putStrLn "Goodbye."
---          return ()
---        Set set -> do
---          let action
---                | set == "stepon" = go env (True, snd flags)
---                | set == "stepoff" = go env (False, snd flags)
---                | set == "normal" = go env (fst flags, "norm")
---                | set == "applicative" = go env (fst flags, "appl")
---                | set == "random" = go env (fst flags, "rand")
---                | otherwise = go env flags
---          action
--- =======
   where
     go :: Env -> Flags -> IO ()
     go env flags = do
@@ -145,7 +96,5 @@ envFromFile name env = do
                                             putStrLn ("Line " ++ (show n) ++ "is not a variable definition. Stopped at line before.")
                                             return [] --stop at error
                   
--- >>>>>>> bcbd6b233be58751df7e16ed7d2bf1b3da76c42a
-
 main :: IO ()
 main = repl
